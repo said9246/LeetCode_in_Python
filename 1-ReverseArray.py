d94e285f0f9bc7337aa0c03a252db5b1ef20c2d5
Said Ahmed <saidahmed9246@gmail.com>
2025-12-01 09:42:06 +0500

    add

--- 1-ReverseArray.py
+++ 1-ReverseArray.py
@@ -1,1 +1,25 @@
-print("working ")
+
+# ---------------------------Reverse Arry---------------------------------------01
+# a=[1,2,4,5,7]
+# a.reverse()
+# print(a)
+
+# -----------custome code
+
+
+# arry=[2,3,4,5,6,7,8]
+# rev=[]
+
+# length=len(arry)-1
+# print(length)
+
+# for i in range(length,-1,-1):
+#     rev.append(arry[i])
+
+# print(rev)
+
+# ---------------------------Find minimum in a Arry---------------------------------------02 
+arry =[4,5,6,1,8,1,2,3,7]
+
+arry.min()
+print("minumum number is ",arry)

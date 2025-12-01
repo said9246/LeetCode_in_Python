9036a243e7ec5f86288224d6f3185bbdc5103510
Said Ahmed <saidahmed9246@gmail.com>
2025-12-01 09:54:21 +0500

    find maximum number

--- 1-ReverseArray.py
+++ 1-ReverseArray.py
@@ -19,7 +19,13 @@
 # print(rev)
 
 # ---------------------------Find minimum in a Arry---------------------------------------02 
-arry =[4,5,6,1,8,1,2,3,7]
+arry =[4,5,6,11,8,12,28,7]
+# print("minumum number is ",min(arry))
 
-arry.min()
-print("minumum number is ",arry)
+minimum =arry[0]
+
+for x in arry:
+    if x > minimum:
+        minimum=x
+
+print("minimum =", minimum)
